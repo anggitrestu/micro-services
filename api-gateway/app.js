35ba2ff1fcbc2fb98468956a0a4f100ebfa66227
anggitrestu <anggitrestu60@gmail.com>
2021-03-28 15:43:08 +0700

    Initial commit

---
+++ api-gateway/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
+const refreshTokensRouter = require('./routes/refreshTokens');
+
+const verifyToken = require('./middlewares/verifyToken');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/courses', verifyToken, coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
+app.use('/refresh-tokens', refreshTokensRouter);
+
+module.exports = app;
